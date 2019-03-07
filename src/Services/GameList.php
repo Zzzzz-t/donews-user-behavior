b420f517497683541af8b9877e16486a539b0bad
田立森 <tianlisen@donews.com>
2019-03-07 12:22:20 +0800

    .

--- src/Services/GameList.php
+++ src/Services/GameList.php
@@ -22,7 +22,7 @@ class GameListModel extends Model implements GameList
     public static function getGameList($keyword, $page, $page_size, $type = 1, $app_id)
     {
         if ($type == 1) {
-            $data = self::select('id', 'uid', 'title', 'content', 'created_at')->where("status", 0)->where("app_name", $app_id)->where('title', $keyword)->offset(($page - 1) * $page_size)->limit($page_size)->get();
+            $data = self::select('id', 'uid', 'title', 'content', 'created_at')->where("status", 0)->where("app_name", $app_id)->where('title', 'like', "%".$keyword."%")->offset(($page - 1) * $page_size)->limit($page_size)->get();
         } else {
             $data = self::select('id', 'uid', 'title', 'content', 'created_at')->where("status", 0)->where("is_system",0)->where("app_name", $app_id)->orderBy("created_at", "desc")->offset(($page - 1) * $page_size)->limit($page_size)->get();
         }
